c5bcb37695789f6d81292b5f8afd8dce37b5bcbe
AidanMountain <34258373+AidanMountain@users.noreply.github.com>
2023-03-02 08:50:41 -0500

    set up the sim

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Welcome to the DMV!");
+        int number = (int)Math.floor(Math.random() * 99);
+        System.out.println("You are number :"+ number);
+        int callingNumber = number +1;
+
+        if(number == 99){
+            callingNumber = 1;
+        }
+        while(callingNumber != number){
+            callingNumber++;
+            System.out.println("Now helping number: "+callingNumber);
+            if(callingNumber > 99){
+                callingNumber = 0;
+            }
+
+        }
+        System.out.println("You idiot, you are missing half of the documents... NEXT!");
     }
 }
